e3c77b71ab82201d4d132724ba6d057545b3d1b8
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2026-05-25 18:59:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function getFormValue(){
+	let first = document.getElementById("fname").value;
+	let last = document.getElementById("lname").value;
+	let phone = document.getElementById("num").value;
+	let email = document.getElementById("email").value;
+	alert(`Fist Name: ${first} +" "+Last Name:${last}+" "+Phone Number:${phone}+" "+Email ID:${email}`);
+}
+
